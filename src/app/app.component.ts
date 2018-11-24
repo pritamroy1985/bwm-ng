346f80a95f778153bc91ad9f7a7625b67edc8093
pritamroy1985 <pritamroy123@hotmail.com>
2018-11-24 16:16:00 +0530

    Header component created basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  componenetVariable = "Here are some links to help you start";
+
+  private clickHandler(): void {
+    alert("Test clcik");
+  }
 }
